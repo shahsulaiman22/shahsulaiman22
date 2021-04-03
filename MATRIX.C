534ea9c87b2bf697757aae8e2077d89174ca5d4d
shahsulaiman22 <80814620+shahsulaiman22@users.noreply.github.com>
2021-04-03 09:52:52 -0700

    Add files via upload

---
+++ MATRIX.C
@@ -0,0 +1,52 @@
+#include <stdio.h>
+int main()
+{
+	int a[2][3],b[2][3],c[2][3],i,j;
+	printf("\n\t\t  ADDITION OF MATRIX");
+	printf("\nENTER VALUES FOR MATRIX A:\n");
+	for(i=0;i<2;i++)
+		for(j=0;j<3;j++)
+			scanf("%d",&a[i][j]);
+	printf("\nENTER VALUES FOR MATRIX B:\n");
+	for(i=0;i<2;i++)
+		for(j=0;j<3;j++)
+			scanf("%d",&b[i][j]);
+	for(i=0;i<2;i++)
+		for(j=0;j<3;j++)
+			c[i][j]=a[i][j]+b[i][j];
+	printf("\nTHE SUM OF MATRIX C ARE:\n");
+	for(i=0;i<2;i++)
+        {
+		for(j=0;j<3;j++)
+			printf("%5d",c[i][j]);
+		printf("\n");
+		printf("\n\t\t PRODUCT OF MATRIX");
+			printf("\nENTER VALUES FOR MATRIX A:\n");
+			for(i=0;i<2;i++)
+				for(j=0;j<3;j++)
+				scanf("%d",&a[i][j]);
+			}
+		printf("\nENTER VALUES FOR MATRIX B:\n");
+		for(i=0;i<2;i++)
+			for(j=0;j<3;j++)
+				scanf("%d",&b[i][j]);
+				for(i=0;i<2;i++)
+				for(j=0;j<3;j++)
+				c[i][j]=a[i][j]*b[i][j];
+		printf("\nTHE product OF MATRIX C ARE:\n");
+		for(i=0;i<2;i++)
+        {
+		
+				for(j=0;j<3;j++)
+				printf("%5d",c[i][j]);
+				printf("\n");
+				return 0;
+				
+		}
+}
+
+	
+	
+
+
+	
